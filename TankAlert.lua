5099a2a8f040ccb6f8065786f06657b899112750
Azuriel-stream <65779985+Azuriel-stream@users.noreply.github.com>
2025-11-02 11:34:32 +0000

    Update TankAlert.lua
    Feat: Add Raid Assist check for channel logic.

--- TankAlert.lua
+++ TankAlert.lua
@@ -1,59 +1,87 @@
 -- Create our main frame
 local tankAlert_Frame = CreateFrame("Frame")
 
+-- --- NEW HELPER FUNCTION (v1.1) ---
+-- This function checks if the player is a raid leader or assist
+function TankAlert_IsRaidAssist()
+    -- Check if we are even in a raid
+    if (GetNumRaidMembers() == 0) then
+        return false
+    end
+
+    -- Get our own name
+    local playerName = UnitName("player")
+
+    -- Loop through the raid roster
+    for i = 1, GetNumRaidMembers() do
+        -- Get the info for this raid member
+        local name, rank = GetRaidRosterInfo(i)
+        
+        -- Check if this member is us
+        if (name == playerName) then
+            -- We found ourselves. Check our rank.
+            -- Rank 2 = Leader, Rank 1 = Assist
+            if (rank == 1 or rank == 2) then
+                return true
+            else
+                -- We are a regular member (rank 0)
+                return false
+            end
+        end
+    end
+    
+    -- Failsafe in case we're not found in the roster (e.g., just joined)
+    return false
+end
+-- --- END OF NEW FUNCTION ---
+
+
 -- Set the script
 tankAlert_Frame:SetScript("OnEvent", function()
 
     -- Check the GLOBAL variable 'event'
     
-    -- This event fires when we log in
     if event == "PLAYER_ENTERING_WORLD" then
-        
-        -- Register for our SINGLE combat event
         tankAlert_Frame:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
-        
-        -- Print a single "loaded" message
         DEFAULT_CHAT_FRAME:AddMessage("|cff00FF00[TankAlert]|r TankAlert is now active.")
-        
-        -- Unregister this event so it only fires once
         tankAlert_Frame:UnregisterEvent("PLAYER_ENTERING_WORLD")
     end
 
     local announceMsg = nil
     
-    -- --- This is our ONLY handler ---
     if event == "CHAT_MSG_SPELL_SELF_DAMAGE" then
-        -- The entire combat log line (e.g., "Your Sunder Armor was dodged...") is in 'arg1'
         local msg = arg1
         
         if (msg) then
-            -- Check for Taunt Resist
             if (string.find(msg, "Taunt") and string.find(msg, "resisted")) then
                 announceMsg = "!!! TAUNT RESISTED !!!"
-                
-            -- Check for Sunder Armor Failure
             elseif (string.find(msg, "Sunder Armor") and (string.find(msg, "dodged") or string.find(msg, "missed") or string.find(msg, "parried"))) then
                 announceMsg = "Sunder Armor FAILED"
-                
-            -- Check for Shield Slam Failure
             elseif (string.find(msg, "Shield Slam") and (string.find(msg, "dodged") or string.find(msg, "missed") or string.find(msg, "parried"))) then
                 announceMsg = "Shield Slam FAILED"
-                
-            -- Check for Revenge Failure
             elseif (string.find(msg, "Revenge") and (string.find(msg, "dodged") or string.find(msg, "missed") or string.find(msg, "parried"))) then
                 announceMsg = "Revenge FAILED"
             end
         end
     end
     
-    -- --- If we found a match, announce it ---
+
+    -- --- UPDATED ANNOUNCEMENT LOGIC (v1.1) ---
     if (announceMsg) then
-        local channel = "SAY"
+        local channel = "SAY" -- Default for solo
+
         if (GetNumRaidMembers() > 0) then
-            channel = "RAID_WARNING"
+            -- We are in a raid. Check our rank.
+            if (TankAlert_IsRaidAssist()) then
+                channel = "RAID_WARNING" -- Use warning if Assist/Leader
+            else
+                channel = "RAID" -- Use normal raid chat if member
+            end
         elseif (GetNumPartyMembers() > 0) then
+            -- We are in a party (but not a raid)
             channel = "PARTY"
         end
+        
         SendChatMessage(announceMsg, channel)
     end
     
